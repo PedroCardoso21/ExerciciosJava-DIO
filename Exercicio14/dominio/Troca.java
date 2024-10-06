a8e7d549d6f71edd33e09e65c2b202a25b3a51e5
Pedro Cardoso <pedrocoscar@gmail.com>
2024-10-06 08:36:42 -0300

    Concluído

---
+++ Exercicio14/dominio/Troca.java
@@ -0,0 +1,33 @@
+package Exercicio14.dominio;
+
+import java.util.Scanner;
+
+public class Troca implements InterfaceTroca {
+    private int A;
+    private int B;
+    private int C;
+
+    Scanner input = new Scanner(System.in);
+
+    public Troca() {
+        System.out.print("Digite o valor de A: ");
+        A = input.nextInt();
+        System.out.print("Digite o valor de B: ");
+        B = input.nextInt();
+    }
+
+    @Override
+    public void trocaDeValores() {
+        C = A;
+        A = B;
+        B = C;
+    }
+
+    @Override
+    public String toString() {
+        return "Troca{" +
+                "A=" + A +
+                ", B=" + B +
+                '}';
+    }
+}
